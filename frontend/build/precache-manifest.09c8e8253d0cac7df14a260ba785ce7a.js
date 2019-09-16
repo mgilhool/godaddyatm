059044d06dd5715741a5cd5a3665e89c5272b20c
Matt Gilhool <mgilhool@gmail.com>
2019-09-16 10:29:17 -0700

    Updated README.md with draft of instructions and added built frontend

---
+++ frontend/build/precache-manifest.09c8e8253d0cac7df14a260ba785ce7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcc8c23509d92a29039895d8d6cfbe6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d42e190705bd884c0d12",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "5e203d2b9fc16cc2cfc8",
+    "url": "/static/css/main.8ca81290.chunk.css"
+  },
+  {
+    "revision": "d42e190705bd884c0d12",
+    "url": "/static/js/2.79f4c9af.chunk.js"
+  },
+  {
+    "revision": "5e203d2b9fc16cc2cfc8",
+    "url": "/static/js/main.1ea06adb.chunk.js"
+  },
+  {
+    "revision": "0207cc1471edc1f9f249",
+    "url": "/static/js/runtime~main.d55c210a.js"
+  }
+]);
